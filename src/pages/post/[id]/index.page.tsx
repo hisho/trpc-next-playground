30227a81ec2f6499a7d673437974471406951660
hisho <hisho.web@gmail.com>
2023-01-16 18:40:23 +0900

    ♻️ post詳細ページをリファクタリング

--- src/pages/post/[id]/index.page.tsx
+++ src/pages/post/[id]/index.page.tsx
@@ -1,46 +1,66 @@
-import { chakra, Heading, Text } from '@chakra-ui/react'
+import { chakra, Heading, Link, Text } from '@chakra-ui/react'
 import type { NextPageWithLayout } from '@src/pages/_app.page'
 import { RouterOutput, trpc } from '@src/utils/trpc'
-import NextError from 'next/error'
+import Head from 'next/head'
+import NextLink from 'next/link'
 import { useRouter } from 'next/router'
+import { z } from 'zod'
 
-type PostByIdOutput = RouterOutput['post']['byId']
+type Props = {
+  post: RouterOutput['post']['byId']
+}
 
-function PostItem(props: { post: PostByIdOutput }) {
-  const { post } = props
+const PostItem = ({ post }: Props) => {
   return (
     <>
       <Heading as={'h1'}>{post.title}</Heading>
       <chakra.em>
-        Created {post.createdAt.toLocaleDateString('en-us')}
+        Created {post.createdAt.toLocaleDateString('ja-JP')}
       </chakra.em>
 
       <Text>{post.text}</Text>
 
       <Heading as={'h2'}>Raw data:</Heading>
-      <chakra.pre>{JSON.stringify(post, null, 4)}</chakra.pre>
+      <chakra.pre>{JSON.stringify(post, null, 2)}</chakra.pre>
     </>
   )
 }
 
+const schema = z.object({
+  id: z.string().catch(''),
+})
+
 const PostViewPage: NextPageWithLayout = () => {
-  const id = useRouter().query['id'] as string
+  const { query } = useRouter()
+  const { id } = schema.parse(query)
   const postQuery = trpc.post.byId.useQuery({ id })
 
   if (postQuery.error) {
     return (
-      <NextError
-        title={postQuery.error.message}
-        statusCode={postQuery.error.data?.httpStatus ?? 500}
-      />
+      <>
+        <Head>
+          <title>404</title>
+        </Head>
+        <div>
+          <Heading>{postQuery.error.data?.httpStatus}</Heading>
+          <Text>{postQuery.error.message}</Text>
+        </div>
+      </>
     )
   }
 
   if (postQuery.status !== 'success') {
     return <>Loading...</>
   }
-  const { data } = postQuery
-  return <PostItem post={data} />
+
+  return (
+    <>
+      <Link as={NextLink} href={'/'}>
+        Back to home
+      </Link>
+      <PostItem post={postQuery.data} />
+    </>
+  )
 }
 
 export default PostViewPage
